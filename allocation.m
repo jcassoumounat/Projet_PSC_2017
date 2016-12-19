669239558b10f1c5988a598924938b823530221a
LelioC <lelio.chetot@gmail.com>
2016-12-19 17:00:46 +0100

    - allocation.m : fixed '[*]' with '(*)' - snr_thresholds added and modified - thresholds added and modified

--- allocation.m
+++ allocation.m
@@ -1,25 +1,25 @@
 function alloc=allocation(ber, gains, bruits, ptot)
     
-    alloc 	= zeros([256, 1]);	% Vecteur contenant le type de modulation qu'il faudra appliquer pour chaque sous-canal
-    alphas 	= zeros([256, 2]);	% Vecteur des rapports bruit à canal
-    R		= ones([256, 2]);	% Vecteur des pourcentages de la puissance totale
-    SNR		= zeros([256, 2]);	% Vecteur des SNR de chaque sous-canal
+    alloc 	= zeros((256, 1));	% Vecteur contenant le type de modulation qu'il faudra appliquer pour chaque sous-canal
+    alphas 	= zeros((256, 2));	% Vecteur des rapports bruit à canal
+    R		= ones((256, 2));	% Vecteur des pourcentages de la puissance totale
+    SNR		= zeros((256, 2));	% Vecteur des SNR de chaque sous-canal
     
     %--- Construction de alphas ---
     for i = 0:256
-    	alphas[i][2]	= bruits[i] / (abs(gains[i])^2 * ptot);
+    	alphas(i)	= bruits(i) / (abs(gains(i))^2 * ptot);
     end
 
-    % On trie alphas pour obtenir A
-    [A, I] = sort(alphas);
+    % On trie alphas pour obtenir A et I, le tableau des index qu'on utilisera ensuite pour réordonner les sous-canaux.
+    (A, I) = sort(alphas);
 
     %--- Construction de R ---
     i = 1
 	while(i <= N && sum <= 1)
-		R[i] = 1/A[i+1][2] - A[i][2]
+		R(i) = 1/A(i+1) - A(i)
 
 		for j = 1:i
-			sum = sum + R[j]
+			sum = sum + R(j)
 		end
 		i = i + 1
 	end
@@ -28,23 +28,24 @@ function alloc=allocation(ber, gains, bruits, ptot)
 		inu = 1
 
 		for j = 1:(i-1) do
-			inu = inu + A[j][2]
+			inu = inu + A(j)
 		end
 
 		for j = 1:(i-1) do
-			R[j] = 1/inu - A[j][2]
+			R(j) = 1/inu - A(j)
 		end
 
 		for j = i:N do
-			R[j] = 0	
+			R(j) = 0	
 		end
 	end
 
+	% On récupère le vecteur des SNR dont le i-ème élément correspond au i-ème sous-canal.
 	SNR = ptot * R(I) .* abs(gains) .* abs(gains) ./ bruits;
 
 	for i = 1:3
-		if (SNR[i] > ber[i])
-			alloc[i] = 2^(i + 1)
+		if (SNR(i) > ber(i))
+			alloc(i) = 2^(i + 1)
 		end
 	end
 end
---
+++ snr_thresholds.m
@@ -0,0 +1,19 @@
+i = 1;
+while ((i <= length(berQAM4)) && (berQAM4(i) > 10^(-7)))
+    i = i + 1;
+end
+threshold_snrQAM4 = snrQAM4(i)
+
+i = 1;
+while ((i <= length(berQAM8)) && (berQAM8(i) > 10^(-7)))
+    i = i + 1;
+end
+threshold_snrQAM8 = snrQAM8(i)
+
+i = 1;
+while ((i <= length(berQAM16)) && (berQAM16(i) > 10^(-7)))
+    i = i + 1;
+end
+threshold_snrQAM16 = snrQAM16(i)
+
+    
