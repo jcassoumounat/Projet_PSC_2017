2b380eb8965b6bafa3a12fad4453290f93cd3fa7
LelioC <lelio.chetot@gmail.com>
2016-12-20 15:43:46 +0100

    - allocation.m : fix some bugs, now it works ! - test_allocation.m modified

--- allocation.m
+++ allocation.m
@@ -1,34 +1,30 @@
 function alloc=allocation(ber, gains, bruits, ptot)
     
     alloc 		= zeros([256, 1]);	% Vecteur contenant le type de modulation qu'il faudra appliquer pour chaque sous-canal
-    alphas 		= zeros([256, 2]);	% Vecteur des rapports bruit à canal
-    alpha_sum 	= zeros([256, 2]);	% Vecteur des sommes des alphas défini de telle façon à ce que alpha_sum(i) = alpha_sum(i) + alphas(i+1)
-    R			= ones([256, 2]);	% Vecteur des pourcentages de la puissance totale
+    alphas 		= zeros([256, 1]);	% Vecteur des rapports bruit à canal
+    R			= ones([256, 1]);	% Vecteur des pourcentages de la puissance totale
     
     %--- Construction de alphas ---
-    for i = 1:256
-    	alphas(i)		= bruits(i) / (abs(gains(i))^2 * ptot);
-    end
-
-    for i = 2:256
-    	alpha_sum(i-1)	= alphas(i) + alpha_sum(i-1)
-    end
-
+    % for i = 1:256
+    % 	alphas(i)	= bruits(i) / (abs(gains(i))^2 * ptot);
+    % end
+    alphas = bruits ./ (ptot * abs(gains) .* abs(gains));
     % On trie alphas pour obtenir A et I, le tableau des index qu'on utilisera ensuite pour réordonner les sous-canaux.
-    A, I = sort(alphas);
+    [A, I] = sort(alphas);
 
     %--- Construction de R ---
     i = 1;
-	while(i <= 256 && alpha_sum(i) <= 1)
+    alpha_sum = 0;
+	while(i <= 255 && alpha_sum <= 1)
 		R(i) = 1/A(i+1) - A(i);
 
 		for j = 1:i
-			sum = sum + R(j);
+			alpha_sum = alpha_sum + R(j);
 		end
 		i = i + 1;
 	end
 
-	if (i <= N)
+	if (i <= 256)
 		inu = 1;
 
 		for j = 1:(i-1)
@@ -39,7 +35,7 @@ function alloc=allocation(ber, gains, bruits, ptot)
 			R(j) = 1/inu - A(j);
 		end
 
-		for j = i:N
+		for j = i:256
 			R(j) = 0;
 		end
 	end
@@ -48,14 +44,16 @@ function alloc=allocation(ber, gains, bruits, ptot)
 	SNR = ptot * R(I) .* abs(gains) .* abs(gains) ./ bruits;
 
 	% On associe le type de modulation correspondant à chaque sous-canal.
-	for i = 1:3
-		if (SNR(i) > ber(i))
-			alloc(i) = 2^(i + 1);
+	for i = 1:256
+		for j = 1:3
+			if (SNR(i) > ber(j))
+				alloc(i) = 2^(j + 1);
+			end
 		end
 	end
 
 	% Si jamais il n'est pas possible de trouver un seuil correspondant, alors on met la modulation la plus basse possible en ADSL ie une 4-QAM.
-	for i = 1:3
+	for i = 1:256
 		if (alloc(i) == 0)
 			alloc(i) = 4;
 		end
--- tests_allocation.m
+++ tests_allocation.m
@@ -1,6 +1,7 @@
-gains	= ones([256,1]);
-bruits  = zeros([256,1]);
+gains	= randi(10, 256, 1);
+bruits  = -randi(5, 256, 1);
 ber 	= thresholds;
-ptot 	= 100;
+ptot 	= 10;
 
-alloc = allocation(ber, gains, bruits, ptot)
+alloc = allocation(ber, gains, bruits, ptot);
+alloc'
