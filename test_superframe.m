fcfa08f72f2a2b9bfd33affe4161bb7f5f2e6367
Hugo Dupré <mr_hogu@hotmail.fr>
2017-01-07 15:23:56 +0100

    Add files via upload

--- test_superframe.m
+++ test_superframe.m
@@ -1,4 +1,4 @@
 data = test_bit_vector(1024);
-rate = 82000;
+rate = 164000;
 [superframe_test, remain] = superframe(data, rate)
-[frame0, frame1, frame34, frame35, superframe] = desuperframe(superframe_test)
+[frame0, frame1, frame34, frame35, superframe_decoded_test] = desuperframe(superframe_test)
