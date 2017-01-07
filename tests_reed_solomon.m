6c22e24b9aa9fd9b9db15338c6a190872dc7c549
LelioC <lelio.chetot@gmail.com>
2017-01-07 13:19:12 +0100

    Delete tests_reed_solomon.m

--- tests_reed_solomon.m
+++
@@ -1,9 +0,0 @@
-n       =   255;
-k       =   8;                          % Codeword and message word lengths
-m       =   8;                          % Number of bits per symbol
-msg     =   gf([5 2 3; 0 1 7],m)        % Two k-symbol message words
-code    =   rsenc(msg,n,k)              % Two n-symbol codewords
- 
-genpoly =   rsgenpoly(n,k)              % Default generator polynomial
-code2   =   rsenc(msg,n,k,genpoly)      % code and code1 are the same codewords
- 
