69452ecbe578e561f63bafd48ec2aad475afc347
LelioC <lelio.chetot@gmail.com>
2017-01-07 13:26:11 +0100

    Update and rename tests_allocation.m.
    New name : script_allocation.m.
    Add the thresholds from script_thresholds directly in this file in order to be more convenient.

---
+++ script_allocation.m
@@ -0,0 +1,17 @@
+% SNR thresholds which grant a probability of error lower than 10^(-7)
+% They have been determined with Matlab's bertool
+threshold_snrQAM4       =    7.8000;    % threshold for a 4-QAM
+
+threshold_snrQAM8       =   10.6000;    % threshold for a 8-QAM
+
+threshold_snrQAM16      =   11.4000;    % threshold for a 16-QAM
+
+thresholds = [threshold_snrQAM4, threshold_snrQAM8, threshold_snrQAM16];
+
+gains	= randi(10, 256, 1);
+bruits  = -randi(5, 256, 1);
+ber 	= thresholds;
+ptot 	= 10;
+
+alloc = allocation(ber, gains, bruits, ptot);
+alloc'
