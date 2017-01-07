c1704329b20880200182b7965bd2bdde552c7639
Jean Cassou-Mounat <jean.cassou.mounat@hotmail.fr>
2017-01-07 16:32:57 +0100

    Delete script_mod_demod_QAM.m

--- script_mod_demod_QAM.m
+++
@@ -1,4 +0,0 @@
-nombre_bits = 9600;
-dataIn = randi([0 1],nombre_bits,3);
-symboles_QAM = modulationQAM(dataIn,nombre_bits,3,[32;32;32]);
-dataOut = demodulationQAM(symboles_QAM,floor(nombre_bits/log2(32)),3,[32;32;32]);
