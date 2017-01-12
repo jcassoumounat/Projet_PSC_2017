a3d8ddfff503be2f89bd35cfeed61ab65436d143
Jean Cassou-Mounat <jean.cassou.mounat@hotmail.fr>
2017-01-12 17:01:07 +0100

    Delete egalisation.m

--- egalisation.m
+++
@@ -1,7 +0,0 @@
-function [signal_after_egalisation] = egalisation(rep_freq)
-    signal_after_egalisation = zeros(length(rep_freq),1);
-    for i =1:length(rep_freq)
-        signal_after_egalisation(i) = rep_freq(i);
-    end
-end
-
