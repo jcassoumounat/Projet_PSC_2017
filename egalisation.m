070c5ea61fb41bf45a0dc0b931a251a190b31279
Jean Cassou-Mounat <jean.cassou.mounat@hotmail.fr>
2017-01-12 19:15:08 +0100

    Add files via upload

--- egalisation.m
+++ egalisation.m
@@ -2,7 +2,7 @@ function [signal_convolved] = egalisation(h_freq, signal_in)
 
 h_inv = zeros(1, length(h_freq));
 for i = 1:length(h_freq)
-    h_inv(i) = 1/h_freq(i);
+    h_inv(i) = conj(h_freq(i))/(real(h_freq(i))^2+imag(h_freq(i))^2);
 end
 signal_convolved = filter(h_inv, 1, signal_in);
 
